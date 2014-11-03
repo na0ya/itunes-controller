6051f77288d7d8127ce76645a6c2218dc4968eb1
na0ya <uoh.org@outlook.com>
2014-11-03 09:25:15 -0800

    Implement the itunes controller methods.

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ itunes-controller
 =================
 
 This helps your language learning style with sublime text.
+
+Setup
+-----------------
+
+The motivation behind this plugin
+-----------------
+
+I'm an English learner. I usually listen some podcasts and I was trying to dictate those podcasts. Unfortunately, as my type spped is slow and my remembering space is not much, I need to back again and again.
--- itunesController.py
+++ itunesController.py
@@ -1,5 +1,54 @@
+# iTunes controlle for dictation.
 import sublime, sublime_plugin
+import sys
+import Pywin32.setup
 
-class ExampleCommand(sublime_plugin.TextCommand):
+platform = sys.platform
+if platform == "win32":
+		import win32com.client
+		iTunes = win32com.client.gencache.EnsureDispatch("iTunes.Application")
+else:
+		from Foundation import *
+		from ScriptingBridge import *
+		iTunes = SBApplication.applicationWithBundleIdentifier_("com.apple.iTunes")
+
+
+class itunes_move_to_next(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.insert(edit, 0, "Hello, World!")
+		if platform == "win32":
+			iTunes.NextTrack()
+		else:
+			iTunes.nextTrack()
+
+class itunes_move_to_previous(sublime_plugin.TextCommand):
+	def run(self, edit):
+		if platform == "win32":
+			iTunes.PreviousTrack()
+		else:
+			iTunes.previousTrack()
+
+class itunes_pause(sublime_plugin.TextCommand):
+	def run(self, edit):
+		if platform == "win32":
+			iTunes.Playpause()
+		else:
+			iTunes.playpause()
+
+class itunes_back(sublime_plugin.TextCommand):
+	def run(self, edit):
+		if platform == "win32":
+			iTunes.Rewind()
+		else:
+			iTunes.rewind()
+
+class itunes_fastforward(sublime_plugin.TextCommand):
+	def run(self, edit):
+		if platform == "win32":
+			iTunes.FastForward()
+		else:
+			iTunes.fastForward()
+
+class itunes_info(sublime_plugin.TextCommand):
+	def runt(self, edit):
+		ls = dir(iTunes)
+		self.view.insert(edit, 50, dir(iTunes))
